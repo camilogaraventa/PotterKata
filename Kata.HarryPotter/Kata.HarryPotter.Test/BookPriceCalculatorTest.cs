1d9d8d3e0344e5ae650d38145bfd761fceb48692
Camilo Garaventa <camilogaraventa@gmail.com>
2016-08-30 20:00:40 -0300

    Zero books costs zero

---
+++ Kata.HarryPotter/Kata.HarryPotter.Test/BookPriceCalculatorTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata.HarryPotter.Test
+{
+    /// <summary>
+    /// Summary description for BookPriceCalculatorTest
+    /// </summary>
+    [TestClass]
+    public class BookPriceCalculatorTest
+    {
+        [TestMethod]
+        public void ZeroBooks_CostsZero()
+        {
+            List<Int32> books = this.GenerateBooksList();
+
+            Decimal cost = BookPriceCalculator.Calculate(books);
+
+            Assert.AreEqual(0, cost);
+
+        }
+     
+        private List<Int32> GenerateBooksList(params Int32[] books)
+        {
+            List<Int32> booksList = new List<Int32>();
+
+            foreach(Int32 book in books)
+            {
+                booksList.Add(book);
+            }
+
+            return booksList;
+        }
+         
+    }
+}
---
+++ Kata.HarryPotter/Kata.HarryPotter/BookPriceCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.HarryPotter
+{
+    public static class BookPriceCalculator
+    {
+        #region Methods
+
+        public static Decimal Calculate(List<Int32> books)
+        {
+            return 0;
+        }
+
+        #endregion
+    }
+}
